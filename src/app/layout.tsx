f0363ae7ae95dda509be4d10f6d877d6f34e077c
matirniy <hmell63@gmail.com>
2025-08-22 01:13:43 +0300

    feature: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
@@ -40,6 +41,7 @@ export default function RootLayout({
           <Footer />
         </div>
       </body>
+      <Analytics />
     </html>
   );
 }
